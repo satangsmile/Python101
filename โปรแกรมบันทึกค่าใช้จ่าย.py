4319d4866324cebdb6ed7c820437db2dd7442ae2
satangsmile <125616251+satangsmile@users.noreply.github.com>
2023-02-20 20:35:49 +0700

    Add files via upload

--- โปรแกรมบันทึกค่าใช้จ่าย.py
+++ โปรแกรมบันทึกค่าใช้จ่าย.py
@@ -14,26 +14,26 @@ L1.place(x=25,y=50)
 #####################################
 
 
-L2 = Label(GUI,text='วันที่',font=('angsana New',20),fg='black')
+L2 = Label(GUI,text='วันที่',font=('angsana New',20),fg='blue')
 L2.place(x=60,y=130)
 E2 = ttk.Entry()
-E2.place(x=160,y=10)
+E2.place(x=160,y=130)
 
 
-L3 = Label(GUI,text='รายการ',font=('angsana New',20),fg='black')
+L3 = Label(GUI,text='รายการ',font=('angsana New',20),fg='blue')
 L3.place(x=60,y=180)
 E3 = ttk.Entry()
 E3.place(x=160,y=180)
 
-L4 = Label(GUI,text='จำนวนเงิน',font=('angsana New',20),fg='black')
+L4 = Label(GUI,text='จำนวนเงิน',font=('angsana New',20),fg='blue')
 L4.place(x=60,y=230)
-L6 = Label(GUI,text='บาท',font=('angsana New',20),fg='black')
+L6 = Label(GUI,text='บาท',font=('angsana New',20),fg='blue')
 L6.place(x=360,y=230)
 E4 = ttk.Entry()
 E4.place(x=160,y=230)
 
 
-L5 = Label(GUI,text='ประเภท',font=('angsana New',20),fg='black')
+L5 = Label(GUI,text='ประเภท',font=('angsana New',20),fg='blue')
 L5.place(x=60,y=280)
 combo1 = ttk.Combobox(value=["ช้อปปิ้ง","อาหาร","ค่าเดินทาง","ของใช้ในบ้าน","อื่นๆ"])
 combo1.place(x=160,y=280)
